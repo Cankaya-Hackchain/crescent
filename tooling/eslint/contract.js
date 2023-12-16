22203f3461f0965558437b4592630b0404249aea
Erhan Özalp <erhanozalp01@gmail.com>
2023-12-16 04:29:44 +0300

    new

--- tooling/eslint/contract.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('eslint').Linter.Config} */
-const config = {
-  root: true,
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
-};
-
-module.exports = config;
